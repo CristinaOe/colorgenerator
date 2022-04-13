1cd8f3b0b8345d926c4b25ed0408c3966b9540e2
Cristina Oechsle <crisoechsle@gmail.com>
2022-04-13 15:19:56 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const colorPicker = document.getElementById("color-picker");
+//console.log(colorPicker)
+const colorMode = document.getElementById("color-mode");
+const schemeBtn = document.getElementById("scheme-btn");
+
+const schemeColor = document.getElementById("scheme-color");
+const hexTextColor = document.getElementById("hex-text-color");
+
+let selectColor = colorPicker.value.slice(1);
+let selectMode = colorMode.value;
+
+let url = `https://www.thecolorapi.com/scheme?hex=${selectColor}&mode=${selectMode}&count=5`;
+
+//console.log(selectMode)
+
+colorPicker.addEventListener("change", function () {
+  selectColor = colorPicker.value.slice(1);
+});
+
+colorMode.addEventListener("change", function () {
+  selectColor = colorMode.value;
+});
+console.log(selectColor);
+
+schemeBtn.addEventListener("click", function () {
+  selectMode = colorMode.value;
+  selectColor = colorPicker.value.slice(1);
+  let url = `https://www.thecolorapi.com/scheme?hex=${selectColor}&mode=${selectMode}&count=5`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const colors = data.colors;
+      let colorOutput = "";
+      let colorHex = "";
+      for (let color of colors) {
+        colorOutput += `
+            <div id="color-output" style="background-color: ${color.hex.value}"></div>`;
+        colorHex += `<p>${color.hex.value}</p>`;
+      }
+      schemeColor.innerHTML = colorOutput;
+      hexTextColor.innerHTML = colorHex;
+    });
+});
